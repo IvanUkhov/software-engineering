59e2799381ce6f1aa721221f5b4ded2bcfb67758
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-27 19:21:22 +0100

    algorithm/sort: Implement bubble sort

--- src/algorithm/sort/bubble.h
+++ src/algorithm/sort/bubble.h
@@ -1,10 +1,26 @@
 #ifndef ALGORITHM_SORT_BUBBLE_H_
 #define ALGORITHM_SORT_BUBBLE_H_
 
+#include <utility>
+
 namespace algorithm { namespace sort {
 
 template <typename T>
 void Bubble(std::vector<int>& data) {
+  typedef typename std::vector<int>::size_type size_t;
+  size_t size = data.size();
+  for (size_t i = 1; i < size; i++) {
+    bool swapped = false;
+    for (size_t j = 0; j < size - i; j++) {
+      if (data[j] > data[j + 1]) {
+        std::swap(data[j], data[j + 1]);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      return;
+    }
+  }
 }
 
 } } // namespace algorithm::sort
