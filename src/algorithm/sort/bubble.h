df9693a894595351a07059401ffc1455a5d0ef23
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-27 19:02:21 +0100

    algorithm/sort: Add a scaffold for bubble sort

---
+++ src/algorithm/sort/bubble.h
@@ -0,0 +1,12 @@
+#ifndef ALGORITHM_SORT_BUBBLE_H_
+#define ALGORITHM_SORT_BUBBLE_H_
+
+namespace algorithm { namespace sort {
+
+template <typename T>
+void Bubble(std::vector<int>& data) {
+}
+
+} } // namespace algorithm::sort
+
+#endif // ALGORITHM_SORT_BUBBLE_H_
---
+++ test/algorithm/sort/bubble.cc
@@ -0,0 +1,19 @@
+#include <algorithm>
+
+#include "fixture.h"
+#include "gtest/gtest.h"
+#include "algorithm/sort/bubble.h"
+
+using namespace algorithm::sort;
+
+const int kCount = 100;
+
+TEST(BubbleTest, Sort) {
+  const std::vector<int> data = fixture::Generate<int>(kCount);
+  std::vector<int> expected(data);
+  std::sort(expected.begin(), expected.end());
+
+  std::vector<int> actual(data);
+  Bubble<int>(actual);
+  ASSERT_EQ(actual, expected);
+}
