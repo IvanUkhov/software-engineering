1d7061022c3289760c1313ee83ec6fc0b86f74cd
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-27 11:02:21 +0100

    search/binary: Complete the implementation

--- src/algorithm/search/binary.h
+++ src/algorithm/search/binary.h
@@ -5,6 +5,18 @@ namespace algorithm { namespace search {
 
 template <typename T>
 int Binary(const std::vector<T>& data, T value) {
+  typename std::vector<T>::size_type size = data.size();
+  int i = 0, j = (int)size;
+  while (i < j) {
+    int k = i + (j - i) / 2;
+    if (data[k] < value) {
+      i = k + 1;
+    } else if (data[k] > value) {
+      j = k;
+    } else {
+      return k;
+    }
+  }
   return -1;
 }
 
