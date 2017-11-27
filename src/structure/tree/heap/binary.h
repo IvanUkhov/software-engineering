55e1683f57ab05782e31924ce85d742965831894
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-27 08:39:12 +0100

    tree/heap: Put Down and Up into a namespace

--- src/structure/tree/heap/binary.h
+++ src/structure/tree/heap/binary.h
@@ -3,63 +3,13 @@
 
 #include <vector>
 
+namespace internal {
+
 template <typename T>
 void Down(std::vector<T>& data, int i, int size);
 
 template <typename T>
 void Up(std::vector<T>& data, int i);
-
-template <typename T>
-class Binary {
- public:
-  typedef std::vector<T> Container;
-  typedef typename Container::size_type SizeType;
-
-  Binary() {}
-  Binary(Container data);
-
-  void Arrange();
-  T Pop();
-  void Push(T value);
-  SizeType Size() const;
-
- private:
-  Container data_;
-};
-
-template <typename T>
-Binary<T>::Binary(Container data) : data_(data) {
-  this->Arrange();
-}
-
-template <typename T>
-void Binary<T>::Arrange() {
-  int size = (int)this->Size();
-  for (int i = (size - 1 - 1) / 2; i >= 0; i--) {
-    Down(this->data_, i, size);
-  }
-}
-
-template <typename T>
-T Binary<T>::Pop() {
-  int size = (int)this->Size() - 1;
-  T value = this->data_[0];
-  this->data_[0] = this->data_[size];
-  this->data_.pop_back();
-  Down(this->data_, 0, size);
-  return value;
-}
-
-template <typename T>
-void Binary<T>::Push(T value) {
-  this->data_.push_back(value);
-  Up(this->data_, (int)this->Size() - 1);
-}
-
-template <typename T>
-typename Binary<T>::SizeType Binary<T>::Size() const {
-  return this->data_.size();
-}
 
 template <typename T>
 void Down(std::vector<T>& data, int i, int size) {
@@ -94,6 +44,60 @@ void Up(std::vector<T>& data, int i) {
     data[j] = value;
     i = j;
   }
+}
+
+} // namespace internal
+
+template <typename T>
+class Binary {
+ public:
+  typedef std::vector<T> Container;
+  typedef typename Container::size_type SizeType;
+
+  Binary() {}
+  Binary(Container data);
+
+  void Arrange();
+  T Pop();
+  void Push(T value);
+  SizeType Size() const;
+
+ private:
+  Container data_;
+};
+
+template <typename T>
+Binary<T>::Binary(Container data) : data_(data) {
+  this->Arrange();
+}
+
+template <typename T>
+void Binary<T>::Arrange() {
+  int size = (int)this->Size();
+  for (int i = (size - 1 - 1) / 2; i >= 0; i--) {
+    internal::Down(this->data_, i, size);
+  }
+}
+
+template <typename T>
+T Binary<T>::Pop() {
+  int size = (int)this->Size() - 1;
+  T value = this->data_[0];
+  this->data_[0] = this->data_[size];
+  this->data_.pop_back();
+  internal::Down(this->data_, 0, size);
+  return value;
+}
+
+template <typename T>
+void Binary<T>::Push(T value) {
+  this->data_.push_back(value);
+  internal::Up(this->data_, (int)this->Size() - 1);
+}
+
+template <typename T>
+typename Binary<T>::SizeType Binary<T>::Size() const {
+  return this->data_.size();
 }
 
 #endif // INTERVIEW_STRUCTURE_TREE_HEAP_BINARY_H_
