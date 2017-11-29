4f1cd3aff4bcb1453e6ecdb8f42537be6ffeaea2
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-29 21:26:25 +0100

    algorithm/sort: Implement quick sort

--- src/algorithm/sort/quick.h
+++ src/algorithm/sort/quick.h
@@ -8,14 +8,39 @@ namespace algorithm { namespace sort {
 namespace internal {
 
 template <typename T>
-void Quick(std::vector<T>& data, int i, int j) {
+int Partition(std::vector<T>& data, int left, int right) {
+  using std::swap;
+  T pivot = data[left];
+  int i = left, j = right;
+  while (true) {
+    while (data[i] < pivot) {
+      ++i;
+    }
+    while (data[j] > pivot) {
+      --j;
+    }
+    if (i < j) {
+      swap(data[i], data[j]);
+    } else {
+      return j;
+    }
+  }
+}
+
+template <typename T>
+void Quick(std::vector<T>& data, int left, int right) {
+  if (left < right) {
+    int middle = Partition(data, left, right);
+    Quick(data, left, middle);
+    Quick(data, middle + 1, right);
+  }
 }
 
 } // namespace internal
 
 template <typename T>
 void Quick(std::vector<T>& data) {
-  internal::Quick(data, 0, data.size());
+  internal::Quick(data, 0, data.size() - 1);
 }
 
 } } // namespace algorithm::sort
