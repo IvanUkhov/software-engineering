a1f81894c12a02df65b4e58ee28faa97ef53e1e2
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-01 15:20:52 +0100

    algorithm/sort: Implement selection sort

--- src/algorithm/sort/selection.h
+++ src/algorithm/sort/selection.h
@@ -7,6 +7,15 @@ namespace algorithm { namespace sort {
 
 template <typename T>
 void Selection(std::vector<T>& data) {
+  using std::swap;
+  auto size = data.size();
+  for (int i = 0; i < size - 1; i++) {
+    int k = i;
+    for (int j = i + 1; j < size; j++) {
+      if (data[k] > data[j]) k = j;
+    }
+    swap(data[i], data[k]);
+  }
 }
 
 } } // namespace algorithm::sort
