fa2208c4ee66417bb397ca43859492b4b138fc24
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-02 21:24:19 +0100

    structure/tree: Begin to implement tries

---
+++ src/structure/tree/trie.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURE_TREE_TRIE_H_
+#define STRUCTURE_TREE_TRIE_H_
+
+namespace structure { namespace tree {
+
+template <typename T>
+class Trie {
+ public:
+};
+
+} } // namespace structure::tree
+
+#endif // STRUCTURE_TREE_TRIE_H_
---
+++ test/structure/tree/trie.cc
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "structure/tree/trie.h"
+
+typedef structure::tree::Trie<int> Tree;
+
+TEST(TreeTest, TrieInsert) {
+  Tree tree;
+}
