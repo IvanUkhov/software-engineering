087e817f9e7665a56676aa805307f9723455a625
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-03 08:05:33 +0100

    structure/tree: Implement the basics of tries

--- src/structure/tree/trie.h
+++ src/structure/tree/trie.h
@@ -1,7 +1,9 @@
 #ifndef STRUCTURE_TREE_TRIE_H_
 #define STRUCTURE_TREE_TRIE_H_
 
+#include <memory>
 #include <string>
+#include <utility>
 
 namespace structure { namespace tree {
 
@@ -9,18 +11,63 @@ template <typename T, T empty = T()>
 class Trie {
  public:
   typedef std::string Key;
+  typedef std::string::value_type Symbol;
+
+  struct Node {
+   public:
+     Node(Symbol symbol = Symbol())
+       : symbol(symbol), value(empty) {}
+
+     T value;
+     Symbol symbol;
+     std::unique_ptr<Node> child;
+     std::unique_ptr<Node> sibling;
+  };
 
   void Insert(const Key& key, T value);
   T Search(const Key& key);
+
+ private:
+  Node root;
 };
 
 template <typename T, T empty>
 void Trie<T, empty>::Insert(const Key& key, T value) {
+  using std::swap;
+  auto current = &root;
+  for (auto symbol : key) {
+    if (!current->child) {
+      auto node = std::unique_ptr<Node>(new Node(symbol));
+      swap(current->child, node);
+      current = current->child.get();
+      continue;
+    }
+    current = current->child.get();
+    while (current->symbol != symbol) {
+      if (!current->sibling) {
+        auto node = std::unique_ptr<Node>(new Node(symbol));
+        swap(current->sibling, node);
+        current = current->sibling.get();
+        break;
+      }
+      current = current->sibling.get();
+    }
+  }
+  swap(current->value, value);
 }
 
 template <typename T, T empty>
 T Trie<T, empty>::Search(const Key& key) {
-  return empty;
+  auto current = &root;
+  for (auto symbol : key) {
+    if (!current->child) return empty;
+    current = current->child.get();
+    while (current->symbol != symbol) {
+      if (!current->sibling) return empty;
+      current = current->sibling.get();
+    }
+  }
+  return current->value;
 }
 
 } } // namespace structure::tree
