c0668e9ad4112c1cd1e9ca6b473aaab636a957b6
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-03 16:31:16 +0100

    structure/map: Add a skeleton for hash map

---
+++ src/structure/map/hash.h
@@ -0,0 +1,12 @@
+#ifndef STRUCTURE_MAP_HASH_H_
+#define STRUCTURE_MAP_HASH_H_
+
+namespace structure { namespace map {
+
+template <typename K, typename V>
+class Hash {
+};
+
+} } // namespace structure::map
+
+#endif // STRUCTURE_MAP_HASH_H_
---
+++ test/structure/map/hash.cc
@@ -0,0 +1,9 @@
+#include <string>
+
+#include "gtest/gtest.h"
+#include "structure/map/hash.h"
+
+typedef structure::map::Hash<std::string, int> Map;
+
+TEST(MapTest, HashInsert) {
+}
