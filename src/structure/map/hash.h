0e9c0927a6a3db8473eb8aa42d6def550d491d04
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-03 19:28:15 +0100

    structure/map: Add a failing test for HashMap

--- src/structure/map/hash.h
+++ src/structure/map/hash.h
@@ -1,11 +1,55 @@
 #ifndef STRUCTURE_MAP_HASH_H_
 #define STRUCTURE_MAP_HASH_H_
 
+#include <cstddef>
+#include <functional>
+#include <memory>
+#include <vector>
+
 namespace structure { namespace map {
 
-template <typename K, typename V>
+template <typename K, typename V, typename hash = std::hash<K>>
 class Hash {
+ public:
+  Hash() : buckets_(1 << 7), size_(0) {}
+
+  void Insert(K key, V value);
+
+  double Load() const {
+    return static_cast<double>(Size()) / static_cast<double>(Breadth());
+  }
+
+  std::size_t Breadth() const {
+    return buckets_.size();
+  }
+
+  std::size_t Size() const {
+    return size_;
+  }
+
+ protected:
+  bool ShouldResize() const {
+    return Load() > 0.8;
+  }
+
+ private:
+  class Bucket {
+  };
+
+  void Resize();
+
+  std::vector<Bucket> buckets_;
+  std::size_t size_;
 };
+
+template <typename K, typename V, typename hash>
+void Hash<K, V, hash>::Insert(K key, V value) {
+  ++size_;
+}
+
+template <typename K, typename V, typename hash>
+void Hash<K, V, hash>::Resize() {
+}
 
 } } // namespace structure::map
 
--- test/structure/map/hash.cc
+++ test/structure/map/hash.cc
@@ -3,7 +3,16 @@
 #include "gtest/gtest.h"
 #include "structure/map/hash.h"
 
+const std::size_t kCount = 100;
+
 typedef structure::map::Hash<std::string, int> Map;
 
 TEST(MapTest, HashInsert) {
+  Map map;
+  map.Insert("key0", 42);
+  for (std::size_t i = 0; i < kCount; i++) {
+    map.Insert("key" + std::to_string(i), i);
+  }
+  map.Insert("key1", 42);
+  ASSERT_EQ(map.Size(), kCount);
 }
