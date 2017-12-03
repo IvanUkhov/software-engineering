ea3440497982aced6408bfc49f92131e8e5a13c6
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-03 20:55:19 +0100

    structure/map: Implement Insert for HashMap

--- src/structure/map/hash.h
+++ src/structure/map/hash.h
@@ -8,7 +8,7 @@
 
 namespace structure { namespace map {
 
-template <typename K, typename V, typename hash = std::hash<K>>
+template <typename K, typename V, typename H = std::hash<K>>
 class Hash {
  public:
   Hash() : buckets_(1 << 7), size_(0) {}
@@ -32,23 +32,61 @@ class Hash {
     return Load() > 0.8;
   }
 
+  std::size_t Index(K key) const {
+    return hash_(std::move(key)) % Breadth();
+  }
+
  private:
+  struct Node {
+   public:
+    Node(K key, V value) : key(std::move(key)), value(std::move(value)) {}
+
+    K key;
+    V value;
+    std::unique_ptr<Node> next;
+  };
+
   class Bucket {
+   public:
+    bool Insert(K key, V value);
+
+   private:
+    std::unique_ptr<Node> root_;
   };
 
   void Resize();
 
+  H hash_;
   std::vector<Bucket> buckets_;
   std::size_t size_;
 };
 
-template <typename K, typename V, typename hash>
-void Hash<K, V, hash>::Insert(K key, V value) {
-  ++size_;
+template <typename K, typename V, typename H>
+void Hash<K, V, H>::Insert(K key, V value) {
+  if (ShouldResize()) Resize();
+  if (buckets_[Index(key)].Insert(std::move(key), std::move(value))) ++size_;
 }
 
-template <typename K, typename V, typename hash>
-void Hash<K, V, hash>::Resize() {
+template <typename K, typename V, typename H>
+void Hash<K, V, H>::Resize() {
+}
+
+template <typename K, typename V, typename H>
+bool Hash<K, V, H>::Bucket::Insert(K key, V value) {
+  using std::swap;
+  std::unique_ptr<Node>* current = &root_;
+  while (true) {
+    std::unique_ptr<Node>& node = *current;
+    if (!node) {
+      node.reset(new Node(std::move(key), std::move(value)));
+      return true;
+    }
+    if (node->key == key) {
+      swap(node->value, value);
+      return false;
+    }
+    current = &node->next;
+  }
 }
 
 } } // namespace structure::map
