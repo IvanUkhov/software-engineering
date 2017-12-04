d68754adefed95859fce494e5dbbc03824c244bf
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-04 14:51:52 +0100

    structure/stack: Implement the array-based stack

--- src/structure/stack/array.h
+++ src/structure/stack/array.h
@@ -1,11 +1,31 @@
 #ifndef STRUCTURE_STACK_ARRAY_H_
 #define STRUCTURE_STACK_ARRAY_H_
 
+#include <cstddef>
+#include <utility>
+#include <vector>
+
 namespace structure { namespace stack {
 
 template <typename T>
 class Array {
  public:
+  void Push(T value) {
+    data_.push_back(std::move(value));
+  }
+
+  T Pop() {
+    T value = std::move(data_.back());
+    data_.pop_back();
+    return value;
+  }
+
+  std::size_t Size() const {
+    return data_.size();
+  }
+
+ private:
+  std::vector<T> data_;
 };
 
 } } // namespace structure::stack
--- test/structure/stack/array.cc
+++ test/structure/stack/array.cc
@@ -1,7 +1,18 @@
 #include "gtest/gtest.h"
 #include "structure/stack/array.h"
 
+const std::size_t kCount = 100;
+
 typedef structure::stack::Array<int> Stack;
 
 TEST(StackTest, ArrayPushPop) {
+  Stack stack;
+  for (int i = 0; i < kCount; ++i) {
+    stack.Push(i);
+  }
+  ASSERT_EQ(stack.Size(), kCount);
+  for (int i = kCount - 1; i >= 0; --i) {
+    ASSERT_EQ(stack.Pop(), i);
+  }
+  ASSERT_EQ(stack.Size(), 0);
 }
