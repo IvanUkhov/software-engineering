646dc1a3afae256e6c1b1b8aa8d8a4fa91efe852
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-04 18:29:56 +0100

    problem: Solve the count-pairs problem

--- src/problem/count_pairs.cc
+++ src/problem/count_pairs.cc
@@ -1,5 +1,15 @@
+#include <unordered_set>
+
 #include "problem/count_pairs.h"
 
-std::vector<std::pair<int, int>> problem::CountPairs(const std::vector<int>& values, int k) {
-  return {};
+std::vector<std::pair<int, int>> problem::CountPairs(
+    const std::vector<int>& values, int k) {
+  std::unordered_set<int> set;
+  std::vector<std::pair<int, int>> result;
+  for (auto value : values) {
+    if (set.count(k + value) > 0) result.push_back({value, k + value});
+    if (set.count(value - k) > 0) result.push_back({value - k, value});
+    set.insert(value);
+  }
+  return result;
 }
--- test/problem/count_pairs.cc
+++ test/problem/count_pairs.cc
@@ -1,8 +1,11 @@
+#include <algorithm>
+
 #include "gtest/gtest.h"
 #include "problem/count_pairs.h"
 
 TEST(ProblemTest, CountPairs) {
   const std::vector<std::pair<int, int>> expected = {{1, 3}, {3, 5}, {5, 7}, {7, 9}};
   auto actual = problem::CountPairs({1, 7, 5, 9, 2, 12, 3}, 2);
+  std::sort(actual.begin(), actual.end());
   ASSERT_EQ(actual, expected);
 }
