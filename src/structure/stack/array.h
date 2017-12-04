025f6c05f2142108f198f156ccdbf03da7da7638
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-04 14:43:16 +0100

    structure/stack: Add a template for array-based stacks

---
+++ src/structure/stack/array.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURE_STACK_ARRAY_H_
+#define STRUCTURE_STACK_ARRAY_H_
+
+namespace structure { namespace stack {
+
+template <typename T>
+class Array {
+ public:
+};
+
+} } // namespace structure::stack
+
+#endif // STRUCTURE_STACK_ARRAY_H_
---
+++ test/structure/stack/array.cc
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "structure/stack/array.h"
+
+typedef structure::stack::Array<int> Stack;
+
+TEST(StackTest, ArrayPushPop) {
+}
