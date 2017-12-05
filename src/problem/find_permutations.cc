41060de6484881f4b914cba176fa8fc2685a33b2
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-05 08:46:00 +0100

    problem: Solve the find-permutations problem

--- src/problem/find_permutations.cc
+++ src/problem/find_permutations.cc
@@ -1,6 +1,16 @@
+#include <algorithm>
+
 #include "problem/find_permutations.h"
 
 std::vector<std::size_t> problem::FindPermutations(std::string s,
                                                    std::string b) {
-  return {};
+  auto ssize = s.size(), bsize = b.size();
+  std::sort(s.begin(), s.end());
+  std::vector<std::size_t> result;
+  for (std::size_t i = 0; i + ssize <= bsize; ++i) {
+    auto candidate = b.substr(i, ssize);
+    std::sort(candidate.begin(), candidate.end());
+    if (s == candidate) result.push_back(i);
+  }
+  return result;
 }
