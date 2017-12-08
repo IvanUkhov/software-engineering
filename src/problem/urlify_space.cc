39fd43e2257a7202517a07d290be848ce4040ccf
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-08 18:52:42 +0100

    problem: Make a cosmetic adjustment

--- src/problem/urlify_space.cc
+++ src/problem/urlify_space.cc
@@ -2,7 +2,9 @@
 
 void problem::UrlifySpace(std::string& string, std::size_t length) {
   int count = 0;
-  for (int i = 0; i < length; ++i) if (string[i] == ' ') ++count;
+  for (int i = 0; i < length; ++i) {
+    if (string[i] == ' ') ++count;
+  }
   int j = (int)length - count + 3 * count - 1;
   for (int i = (int)length - 1; i >= 0; --i) {
     if (string[i] != ' ') {
