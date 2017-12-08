9751db6a7ba822caa5393a6f000dc527a385cbdb
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-08 19:46:05 +0100

    problem: Solve the check-palindrome problem

--- src/problem/check_palindrome.cc
+++ src/problem/check_palindrome.cc
@@ -1,5 +1,17 @@
+#include <cstddef>
+
 #include "problem/check_palindrome.h"
 
 bool problem::CheckPalindrome(const std::string& string) {
-  return false;
+  std::size_t counts[26] = {};
+  for (auto glyph : string) {
+    if (glyph >= 'A' && glyph <= 'Z') ++counts[glyph - 'A'];
+    else if (glyph >= 'a' && glyph <= 'z') ++counts[glyph - 'a'];
+  }
+  std::size_t odds = 0;
+  for (auto count : counts) {
+    if (count % 2 == 1) ++odds;
+    if (odds > 1) return false;
+  }
+  return true;
 }
