1fcd5a8548531cfa3b7fea6a84c6bb1db3e6bd17
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-08 21:38:34 +0100

    problem: Solve the rotate-matrix problem

--- src/problem/rotate_matrix.h
+++ src/problem/rotate_matrix.h
@@ -10,6 +10,18 @@ namespace problem {
 // place?
 template <typename T>
 void RotateMatrix(T** data, std::size_t size) {
+  for (std::size_t i1 = 0; i1 < size; ++i1) {
+    for (std::size_t j1 = i1; j1 < size - i1 - 1; ++j1) {
+      int i2 = j1, j2 = size - i1 - 1;
+      int i3 = j2, j3 = size - i2 - 1;
+      int i4 = j3, j4 = size - i3 - 1;
+      T value = data[i1][j1];
+      data[i1][j1] = data[i4][j4];
+      data[i4][j4] = data[i3][j3];
+      data[i3][j3] = data[i2][j2];
+      data[i2][j2] = value;
+    }
+  }
 }
 
 } // namespace problem
--- test/problem/rotate_matrix.cc
+++ test/problem/rotate_matrix.cc
@@ -1,21 +1,33 @@
+#include <iostream>
+
 #include "gtest/gtest.h"
 #include "problem/rotate_matrix.h"
 
-const std::size_t kSize = 10;
+namespace internal {
 
-TEST(ProblemTest, RotateMatrix) {
-  int plain_data[kSize][kSize];
-  int* data[kSize];
-  for (std::size_t i = 0; i < kSize; ++i) data[i] = plain_data[i];
-  for (std::size_t i = 0; i < kSize; ++i) {
-    for (std::size_t j = 0; j < kSize; ++j) {
+using std::size_t;
+
+template <size_t size>
+void Test() {
+  int plain_data[size][size];
+  int* data[size];
+  for (size_t i = 0; i < size; ++i) data[i] = plain_data[i];
+  for (size_t i = 0; i < size; ++i) {
+    for (size_t j = 0; j < size; ++j) {
       data[i][j] = (i + 1) * (j + 1);
     }
   }
-  problem::RotateMatrix(data, kSize);
-  for (std::size_t i = 0; i < kSize; ++i) {
-    for (std::size_t j = 0; j < kSize; ++j) {
-      ASSERT_EQ(data[j][kSize - i - 1], (i + 1) * (j + 1));
+  problem::RotateMatrix(data, size);
+  for (size_t i = 0; i < size; ++i) {
+    for (size_t j = 0; j < size; ++j) {
+      ASSERT_EQ(data[j][size - i - 1], (i + 1) * (j + 1));
     }
   }
+}
+
+} // namespace internal
+
+TEST(ProblemTest, RotateMatrix) {
+  internal::Test<10>();
+  internal::Test<11>();
 }
