d4eb1328c423a50e70959fafcc289e7d5b65971c
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-12 20:42:54 +0100

    algorithm/search: Add a type check

--- src/algorithm/search/dijkstra.h
+++ src/algorithm/search/dijkstra.h
@@ -1,17 +1,24 @@
 #ifndef ALGORITHM_SEARCH_DIJKSTRA_H_
 #define ALGORITHM_SEARCH_DIJKSTRA_H_
 
+#include <type_traits>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
 namespace algorithm { namespace search {
 
-template <typename G,
-          typename N = typename G::Node,
-          typename E = typename G::Edge>
-std::vector<std::pair<N*, E*>> Dijkstra(const G& graph,
-                                        const N& from,
-                                        const N& into) {
+template <typename Graph,
+          typename Node = typename Graph::Node,
+          typename Edge = typename Graph::Edge>
+std::vector<std::pair<Node*, Edge*>> Dijkstra(const Graph& graph,
+                                              const Node& from,
+                                              const Node& into) {
+  static_assert(std::is_unsigned<typename std::remove_reference<decltype(
+                    std::declval<Edge>().Value())>::type>::value,
+                "Weights should be non-negative");
+  std::unordered_map<Node*, std::size_t> shortest;
+  std::unordered_map<Node*, Node*> previous;
   return {};
 }
 
