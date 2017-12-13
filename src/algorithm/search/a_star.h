1496312f08c05f3035427860a9161ced13eabf05
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-13 18:19:38 +0100

    algorithm/search: Prepare for A*

---
+++ src/algorithm/search/a_star.h
@@ -0,0 +1,8 @@
+#ifndef ALGORITHM_SEARCH_A_STAR_H_
+#define ALGORITHM_SEARCH_A_STAR_H_
+
+namespace algorithm { namespace search {
+
+} } // namespace algorithm::search
+
+#endif // ALGORITHM_SEARCH_A_STAR_H_
---
+++ test/algorithm/search/a_star.cc
@@ -0,0 +1,5 @@
+#include "algorithm/search/a_star.h"
+#include "gtest/gtest.h"
+
+TEST(SearchTest, AStar) {
+}
