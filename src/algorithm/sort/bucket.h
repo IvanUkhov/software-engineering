e1c4bf1710e2110e7f04235fa02e9c1a2d1dba9b
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-14 21:19:05 +0100

    algorithm/sort: Implement bucket sort

--- src/algorithm/sort/bucket.h
+++ src/algorithm/sort/bucket.h
@@ -1,15 +1,28 @@
 #ifndef ALGORITHM_SORT_BUCKET_H_
 #define ALGORITHM_SORT_BUCKET_H_
 
+#include <algorithm>
 #include <cstddef>
 #include <functional>
 #include <vector>
+
+#include "algorithm/sort/insertion.h"
 
 namespace algorithm { namespace sort {
 
 template <typename T>
 void Bucket(std::vector<T>& data, std::size_t granularity,
             std::function<std::size_t(const T&)> map) {
+  std::vector<std::vector<T>> buckets(granularity);
+  for (auto value : data) {
+    buckets[map(value) % granularity].push_back(value);
+  }
+  auto iterator = data.begin();
+  for (auto& bucket : buckets) {
+    Insertion(bucket);
+    std::copy(bucket.begin(), bucket.end(), iterator);
+    iterator += bucket.size();
+  }
 }
 
 } } // namespace algorithm::sort
