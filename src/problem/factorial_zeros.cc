a2e6beb87681211d0945e0ec4396ac6d8db242a4
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-15 21:27:59 +0100

    problem: Optimize the factorial-zeros problem

--- src/problem/factorial_zeros.cc
+++ src/problem/factorial_zeros.cc
@@ -10,10 +10,7 @@ std::size_t Count(std::size_t dividend, std::size_t dividor) {
 }
 
 std::size_t problem::FactorialZeros(std::size_t n) {
-  std::size_t two = 0, five = 0;
-  for (std::size_t i = 1; i <= n; ++i) {
-    two += Count(i, 2);
-    five += Count(i, 5);
-  }
-  return (two < five) ? two : five;
+  std::size_t count = 0;
+  for (std::size_t i = 5; i <= n; ++i) count += Count(i, 5);
+  return count;
 }
