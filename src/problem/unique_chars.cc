62b5b763544a8aa215ef11bae8d89d3991de9424
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-22 09:39:33 +0100

    problem: Solve the unique-chars problem

--- src/problem/unique_chars.cc
+++ src/problem/unique_chars.cc
@@ -1,5 +1,12 @@
+#include <vector>
+
 #include "problem/unique_chars.h"
 
 bool problem::UniqueChars(const std::string& string, char begin, char end) {
-  return false;
+  std::vector<bool> seen(end - begin);
+  for (auto glyph : string) {
+    if (seen[glyph - begin]) return false;
+    seen[glyph - begin] = true;
+  }
+  return true;
 }
