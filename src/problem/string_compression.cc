4624a7ceb41553168ec4f92265545f7ccdf2f0a2
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-22 17:50:39 +0100

    problem: Solve the string-compression problem

--- src/problem/string_compression.cc
+++ src/problem/string_compression.cc
@@ -1,5 +1,17 @@
+#include <cstddef>
+#include <sstream>
+
 #include "problem/string_compression.h"
 
 std::string problem::StringCompression(const std::string& string) {
-  return "";
+  auto size = string.size();
+  std::stringstream result;
+  for (std::size_t i = 1, count = 1; i <= size; ++i, ++count) {
+    if (i == size || string[i - 1] != string[i]) {
+      result << string[i - 1] << count;
+      if (result.tellg() > size) return string;
+      count = 0;
+    }
+  }
+  return result.str();
 }
