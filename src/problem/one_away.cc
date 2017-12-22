f4a80be4697245330310be6928d39b726208db4f
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-22 12:55:41 +0100

    problem: Solve the one-away problem

--- src/problem/one_away.cc
+++ src/problem/one_away.cc
@@ -1,5 +1,36 @@
+#include <cstddef>
+
 #include "problem/one_away.h"
 
+bool OneAway(const std::string& a, const std::string& b) {
+  auto a_size = a.size();
+  auto b_size = b.size();
+  switch (b_size - a_size) {
+    case 0: {
+      for (std::size_t i = 0, count = 0; i < a_size; ++i) {
+        if (a[i] != b[i]) {
+          if (++count > 1) return false;
+        }
+      }
+      return true;
+    }
+    case 1: {
+      for (std::size_t i = 0, j = 0, count = 0; j < b_size; ++j) {
+        if (a[i] != b[j]) {
+          if (++count > 1) return false;
+        } else {
+          ++i;
+        }
+      }
+      return true;
+    }
+    default: {
+      return false;
+    }
+  }
+}
+
 bool problem::OneAway(const std::string& one, const std::string& other) {
-  return true;
+  if (one.size() < other.size()) return ::OneAway(one, other);
+  else return ::OneAway(other, one);
 }
--- test/problem/one_away.cc
+++ test/problem/one_away.cc
@@ -2,8 +2,10 @@
 #include "problem/one_away.h"
 
 TEST(ProblemTest, OneAway) {
-  ASSERT_TRUE(problem::OneAway("pale", "ple"));
-  ASSERT_TRUE(problem::OneAway("pales", "pale"));
   ASSERT_TRUE(problem::OneAway("pale", "bale"));
+  ASSERT_TRUE(problem::OneAway("pale", "pale"));
+  ASSERT_TRUE(problem::OneAway("pale", "pales"));
+  ASSERT_TRUE(problem::OneAway("pale", "ple"));
   ASSERT_FALSE(problem::OneAway("pale", "bake"));
+  ASSERT_FALSE(problem::OneAway("pale", "paleee"));
 }
