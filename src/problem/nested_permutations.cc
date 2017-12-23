a12e0138d280cb9763dcf2553d6cb12d2e2c5833
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-23 11:41:38 +0100

    problem: Make another cosmetic adjustment

--- src/problem/nested_permutations.cc
+++ src/problem/nested_permutations.cc
@@ -2,15 +2,15 @@
 
 #include "problem/nested_permutations.h"
 
-std::vector<std::size_t> problem::NestedPermutations(std::string s,
-                                                     std::string b) {
-  auto ssize = s.size(), bsize = b.size();
-  std::sort(s.begin(), s.end());
+std::vector<std::size_t> problem::NestedPermutations(std::string small,
+                                                     std::string big) {
+  auto small_size = small.size(), big_size = big.size();
+  std::sort(small.begin(), small.end());
   std::vector<std::size_t> result;
-  for (std::size_t i = 0; i + ssize <= bsize; ++i) {
-    auto candidate = b.substr(i, ssize);
+  for (std::size_t i = 0; i + small_size <= big_size; ++i) {
+    auto candidate = big.substr(i, small_size);
     std::sort(candidate.begin(), candidate.end());
-    if (s == candidate) result.push_back(i);
+    if (small == candidate) result.push_back(i);
   }
   return result;
 }
--- src/problem/nested_permutations.h
+++ src/problem/nested_permutations.h
@@ -10,7 +10,7 @@ namespace problem {
 // Given a smaller string s and a bigger string b, design an algorithm to find
 // all permutations of the shorter string within the longer one. Print the
 // location of each permutation.
-std::vector<std::size_t> NestedPermutations(std::string s, std::string b);
+std::vector<std::size_t> NestedPermutations(std::string small, std::string big);
 
 } // namespace problem
 
