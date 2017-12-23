2e54021298f87bce8918bcfda53ecb1a1d375814
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-23 11:26:32 +0100

    problem: Revisit the solution to the zero-matrix problem

--- src/problem/zero_matrix.h
+++ src/problem/zero_matrix.h
@@ -1,7 +1,7 @@
 #ifndef PROBLEM_ZERO_MATRIX_H_
 #define PROBLEM_ZERO_MATRIX_H_
 
-#include <unordered_set>
+#include <vector>
 
 namespace problem {
 
@@ -9,24 +9,19 @@ namespace problem {
 // row and column are set to zero.
 template <typename T>
 void ZeroMatrix(std::vector<T>& data, std::size_t rows, std::size_t columns) {
-  std::unordered_set<std::size_t> zero_rows;
-  std::unordered_set<std::size_t> zero_columns;
+  std::vector<bool> zero_rows(rows);
+  std::vector<bool> zero_columns(columns);
   for (std::size_t i = 0; i < rows; ++i) {
     for (std::size_t j = 0; j < columns; ++j) {
       if (data[i * columns + j] == 0) {
-        zero_rows.insert(i);
-        zero_columns.insert(j);
+        zero_rows[i] = true;
+        zero_columns[j] = true;
       }
     }
   }
-  for (auto i : zero_rows) {
+  for (std::size_t i = 0; i < rows; ++i) {
     for (std::size_t j = 0; j < columns; ++j) {
-      data[i * columns + j] = 0;
-    }
-  }
-  for (auto j : zero_columns) {
-    for (std::size_t i = 0; i < rows; ++i) {
-      data[i * columns + j] = 0;
+      if (zero_rows[i] || zero_columns[j]) data[i * columns + j] = 0;
     }
   }
 }
