fc4464f79a81a7ea5c8817845b159d29ade790c3
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-23 10:08:01 +0100

    problem: Switch to std::vector for representing matrices

--- src/problem/matrix_rotation.h
+++ src/problem/matrix_rotation.h
@@ -2,6 +2,7 @@
 #define PROBLEM_MATRIX_ROTATION_H_
 
 #include <cstddef>
+#include <vector>
 
 namespace problem {
 
@@ -9,17 +10,17 @@ namespace problem {
 // 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in
 // place?
 template <typename T>
-void MatrixRotation(T** data, std::size_t size) {
+void MatrixRotation(std::vector<T>& data, std::size_t size) {
   for (std::size_t i1 = 0; i1 < size; ++i1) {
     for (std::size_t j1 = i1; j1 < size - i1 - 1; ++j1) {
       int i2 = j1, j2 = size - i1 - 1;
       int i3 = j2, j3 = size - i2 - 1;
       int i4 = j3, j4 = size - i3 - 1;
-      T value = data[i1][j1];
-      data[i1][j1] = data[i4][j4];
-      data[i4][j4] = data[i3][j3];
-      data[i3][j3] = data[i2][j2];
-      data[i2][j2] = value;
+      T value = data[i1 * size + j1];
+      data[i1 * size + j1] = data[i4 * size + j4];
+      data[i4 * size + j4] = data[i3 * size + j3];
+      data[i3 * size + j3] = data[i2 * size + j2];
+      data[i2 * size + j2] = value;
     }
   }
 }
--- test/problem/matrix_rotation.cc
+++ test/problem/matrix_rotation.cc
@@ -7,18 +7,16 @@ using std::size_t;
 
 template <size_t size>
 void Test() {
-  int plain_data[size][size];
-  int* data[size];
-  for (size_t i = 0; i < size; ++i) data[i] = plain_data[i];
+  std::vector<int> data(size * size);
   for (size_t i = 0; i < size; ++i) {
     for (size_t j = 0; j < size; ++j) {
-      data[i][j] = (i + 1) * (j + 1);
+      data[i * size + j] = (i + 1) * (j + 1);
     }
   }
   problem::MatrixRotation(data, size);
   for (size_t i = 0; i < size; ++i) {
     for (size_t j = 0; j < size; ++j) {
-      ASSERT_EQ(data[j][size - i - 1], (i + 1) * (j + 1));
+      ASSERT_EQ(data[j * size + size - i - 1], (i + 1) * (j + 1));
     }
   }
 }
