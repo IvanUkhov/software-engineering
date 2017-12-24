9d5b2a4f689b8b767bdc5ef1558164197438627f
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-24 14:52:30 +0100

    problem: Solve the common-subsequence problem inefficiently

--- src/problem/common_subsequence.h
+++ src/problem/common_subsequence.h
@@ -1,6 +1,7 @@
 #ifndef PROBLEM_COMMON_SUBSEQUENCE_H_
 #define PROBLEM_COMMON_SUBSEQUENCE_H_
 
+#include <cstddef>
 #include <utility>
 #include <vector>
 
@@ -10,7 +11,26 @@ namespace problem {
 template <typename T>
 std::pair<int, int> CommonSubsequence(const std::vector<T>& one,
                                       const std::vector<T>& other) {
-  return {-1, -1};
+  auto one_size = one.size();
+  auto other_size = other.size();
+  auto overlap = [&](std::size_t i, std::size_t j) {
+    std::size_t count = 0;
+    while (i < one_size && j < other_size && one[i++] == other[j++]) ++count;
+    return count;
+  };
+  auto result = std::make_pair(-1, -1);
+  int max_count = 0;
+  for (std::size_t i = 0; i < one_size; ++i) {
+    for (std::size_t j = 0; j < other_size; ++j) {
+      auto count = overlap(i, j);
+      if (count > max_count) {
+        max_count = count;
+        result.first = i;
+        result.second = j;
+      }
+    }
+  }
+  return result;
 }
 
 } // namespace problem
