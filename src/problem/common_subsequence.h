5db8566ce0740d7dbcef21e23055945ddb0f6b1d
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-24 15:11:37 +0100

    problem: Introduce caching into the common-subsequence problem

--- src/problem/common_subsequence.h
+++ src/problem/common_subsequence.h
@@ -11,23 +11,39 @@ namespace problem {
 template <typename T>
 std::pair<int, int> CommonSubsequence(const std::vector<T>& one,
                                       const std::vector<T>& other) {
-  auto one_size = one.size();
-  auto other_size = other.size();
-  auto overlap = [&](std::size_t i, std::size_t j) {
-    std::size_t count = 0;
-    while (i < one_size && j < other_size && one[i++] == other[j++]) ++count;
-    return count;
+  struct Problem {
+    const std::vector<T>& one;
+    const std::vector<T>& other;
+    const std::size_t one_size;
+    const std::size_t other_size;
+    std::vector<int> solutions;
+
+    Problem(const std::vector<T>& one, const std::vector<T>& other)
+        : one(one),
+          other(other),
+          one_size(one.size()),
+          other_size(other.size()),
+          solutions(one_size * other_size, -1) {
+    }
+
+    std::size_t Compute(std::size_t i, std::size_t j) {
+      auto k = i * other_size + j;
+      if (k > one_size * other_size) return 0;
+      if (solutions[k] != -1) return solutions[k];
+      solutions[k] = (one[i] == other[j]) ? 1 + Compute(i + 1, j + 1) : 0;
+      return solutions[k];
+    }
   };
+  Problem problem(one, other);
+  std::size_t max_count = 0;
   auto result = std::make_pair(-1, -1);
-  int max_count = 0;
-  for (std::size_t i = 0; i < one_size; ++i) {
-    for (std::size_t j = 0; j < other_size; ++j) {
-      auto count = overlap(i, j);
-      if (count > max_count) {
-        max_count = count;
-        result.first = i;
-        result.second = j;
-      }
+  for (std::size_t i = 0; i + max_count < problem.one_size; ++i) {
+    for (std::size_t j = 0; j + max_count < problem.other_size; ++j) {
+      auto count = problem.Compute(i, j);
+      if (count <= max_count) continue;
+      max_count = count;
+      result.first = i;
+      result.second = j;
     }
   }
   return result;
