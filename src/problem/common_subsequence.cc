ae3bb3b24e2fd36789e39e134d9e350f993f3085
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-24 17:17:52 +0100

    problem: Rewrite the solution to the common-subsequence problem

---
+++ src/problem/common_subsequence.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cstddef>
+#include <vector>
+
+#include "problem/common_subsequence.h"
+
+std::vector<std::size_t> Evaluate(const std::string& one,
+                                  const std::string& other) {
+  auto rows = one.size() + 1, columns = other.size() + 1;
+  std::vector<std::size_t> scores(rows * columns, 0);
+  for (std::size_t i = 1; i < rows; ++i) {
+    for (std::size_t j = 1; j < columns; ++j) {
+      auto& size = scores[i * columns + j];
+      if (one[i - 1] == other[j - 1]) {
+        auto diagonal = scores[(i - 1) * columns + (j - 1)];
+        size = diagonal + 1;
+      } else {
+        auto horizontal = scores[i * columns + (j - 1)];
+        auto vertical = scores[(i - 1) * columns + j];
+        size = std::max(horizontal, vertical);
+      }
+    }
+  }
+  return scores;
+}
+
+std::string Read(const std::string& one, const std::string& other,
+                 const std::vector<std::size_t> scores) {
+  auto rows = one.size() + 1, columns = other.size() + 1;
+  auto i = rows - 1, j = columns - 1;
+  std::string result;
+  while (i > 0 && j > 0) {
+    if (one[i - 1] == other[j - 1]) {
+      result = one[i - 1] + result;
+      --i, --j;
+    } else {
+      auto horizontal = scores[(i - 1) * columns + j];
+      auto vertical = scores[i * columns + (j - 1)];
+      horizontal < vertical ? --j : --i;
+    }
+  }
+  return result;
+}
+
+std::string problem::CommonSubsequence(const std::string& one,
+                                       const std::string& other) {
+  return Read(one, other, Evaluate(one, other));
+}
--- src/problem/common_subsequence.h
+++ src/problem/common_subsequence.h
@@ -1,53 +1,12 @@
 #ifndef PROBLEM_COMMON_SUBSEQUENCE_H_
 #define PROBLEM_COMMON_SUBSEQUENCE_H_
 
-#include <cstddef>
-#include <utility>
-#include <vector>
+#include <string>
 
 namespace problem {
 
 // Find the longest common subsequence between two arrays.
-template <typename T>
-std::pair<int, int> CommonSubsequence(const std::vector<T>& one,
-                                      const std::vector<T>& other) {
-  struct Problem {
-    const std::vector<T>& one;
-    const std::vector<T>& other;
-    const std::size_t one_size;
-    const std::size_t other_size;
-    std::vector<int> solutions;
-
-    Problem(const std::vector<T>& one, const std::vector<T>& other)
-        : one(one),
-          other(other),
-          one_size(one.size()),
-          other_size(other.size()),
-          solutions(one_size * other_size, -1) {
-    }
-
-    std::size_t Compute(std::size_t i, std::size_t j) {
-      auto k = i * other_size + j;
-      if (k > one_size * other_size) return 0;
-      if (solutions[k] != -1) return solutions[k];
-      solutions[k] = (one[i] == other[j]) ? 1 + Compute(i + 1, j + 1) : 0;
-      return solutions[k];
-    }
-  };
-  Problem problem(one, other);
-  std::size_t max_count = 0;
-  auto result = std::make_pair(-1, -1);
-  for (std::size_t i = 0; i + max_count < problem.one_size; ++i) {
-    for (std::size_t j = 0; j + max_count < problem.other_size; ++j) {
-      auto count = problem.Compute(i, j);
-      if (count <= max_count) continue;
-      max_count = count;
-      result.first = i;
-      result.second = j;
-    }
-  }
-  return result;
-}
+std::string CommonSubsequence(const std::string& one, const std::string& other);
 
 } // namespace problem
 
