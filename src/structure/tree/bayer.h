dee5e9f65de2369c580e67c51e4ebfac02e33cf1
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-25 13:39:05 +0100

    structure/tree: Start to implement Bayer

--- src/structure/tree/bayer.h
+++ src/structure/tree/bayer.h
@@ -1,11 +1,43 @@
 #ifndef STRUCTURE_TREE_BAYER_H_
 #define STRUCTURE_TREE_BAYER_H_
 
+#include <array>
+#include <cstddef>
+#include <memory>
+
 namespace structure { namespace tree {
 
-template <typename T>
+template <typename K, typename V, std::size_t N>
 class Bayer {
+ public:
+  class Node {
+    friend class Bayer;
+
+   public:
+
+   private:
+    Node() : leaf_(true), size_(0) {}
+
+    bool leaf_;
+    std::size_t size_;
+    std::array<K, 2 * N - 1> keys_;
+    std::array<V, 2 * N - 1> values_;
+    std::array<std::unique_ptr<Node>, 2 * N> children_;
+  };
+
+  Bayer() {}
+
+  void Insert(K key, V value);
+
+ private:
+  std::unique_ptr<Node> root_;
 };
+
+template <typename K, typename V, std::size_t N>
+void Bayer<K, V, N>::Insert(K key, V value) {
+  auto& current = &root_;
+  current = std::unique_ptr<Node>(new Node());
+}
 
 } } // namespace structure::tree
 
--- test/structure/tree/bayer.cc
+++ test/structure/tree/bayer.cc
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "structure/tree/bayer.h"
 
-using Tree = structure::tree::Bayer<int>;
+using Tree = structure::tree::Bayer<int, int, 2>;
 
 TEST(TreeTest, Bayer) {
   Tree tree;
