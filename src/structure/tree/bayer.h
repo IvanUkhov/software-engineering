4ac55c59090226e766b697a0cbad23dcd3587496
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-25 06:19:59 +0100

    structure/tree: Prepare for implementing B trees

---
+++ src/structure/tree/bayer.h
@@ -0,0 +1,12 @@
+#ifndef STRUCTURE_TREE_BAYER_H_
+#define STRUCTURE_TREE_BAYER_H_
+
+namespace structure { namespace tree {
+
+template <typename T>
+class Bayer {
+};
+
+} } // namespace structure::tree
+
+#endif // STRUCTURE_TREE_BAYER_H_
---
+++ test/structure/tree/bayer.cc
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "structure/tree/bayer.h"
+
+using Tree = structure::tree::Bayer<int>;
+
+TEST(TreeTest, Bayer) {
+  Tree tree;
+}
