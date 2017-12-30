311f73db4371ebfdf3fd84a0678b99c7ac91fbad
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-30 17:28:29 +0100

    test: Rename a couple of tests

--- test/problem/balanced_tree.cc
+++ test/problem/balanced_tree.cc
@@ -4,30 +4,29 @@
 
 using Tree = structure::tree::BinarySearch<int>;
 
-TEST(ProblemTest, BalancedTree) {
-  {
-    Tree tree;
-    tree.Insert(8)
-        .Insert(3)
-        .Insert(1)
-        .Insert(6)
-        .Insert(4)
-        .Insert(7)
-        .Insert(10)
-        .Insert(14)
-        .Insert(13);
-    ASSERT_FALSE(problem::BalancedTree(tree));
-  }
-  {
-    Tree tree;
-    tree.Insert(8)
-        .Insert(3)
-        .Insert(1)
-        .Insert(6)
-        .Insert(4)
-        .Insert(7)
-        .Insert(10)
-        .Insert(9);
-    ASSERT_TRUE(problem::BalancedTree(tree));
-  }
+TEST(ProblemTest, BalancedTreeFailure) {
+  Tree tree;
+  tree.Insert(8)
+      .Insert(3)
+      .Insert(1)
+      .Insert(6)
+      .Insert(4)
+      .Insert(7)
+      .Insert(10)
+      .Insert(14)
+      .Insert(13);
+  ASSERT_FALSE(problem::BalancedTree(tree));
+}
+
+TEST(ProblemTest, BalancedTreeSuccess) {
+  Tree tree;
+  tree.Insert(8)
+      .Insert(3)
+      .Insert(1)
+      .Insert(6)
+      .Insert(4)
+      .Insert(7)
+      .Insert(10)
+      .Insert(9);
+  ASSERT_TRUE(problem::BalancedTree(tree));
 }
