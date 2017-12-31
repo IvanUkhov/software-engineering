c14b3fe58a62eee272c77bbde5b98bd057f7f76e
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-31 16:07:37 +0100

    problem: Simplify the binary-search problem

--- src/problem/binary_search.h
+++ src/problem/binary_search.h
@@ -1,31 +1,17 @@
 #ifndef PROBLEM_BINARY_SEARCH_H_
 #define PROBLEM_BINARY_SEARCH_H_
 
-#include <algorithm>
-#include <utility>
-
 namespace problem {
 
 namespace internal {
 
 template <typename T, typename U>
-std::pair<bool, std::pair<T, T>> BinarySearch(const U* node) {
-  auto range = std::make_pair(node->Value(), node->Value());
-  if (node->Left()) {
-    auto other = BinarySearch<T, U>(node->Left());
-    if (!other.first) return other;
-    if (other.second.second > node->Value()) return {false, {}};
-    range.first = std::min(range.first, other.second.first);
-    range.second = std::max(range.second, other.second.second);
-  }
-  if (node->Right()) {
-    auto other = BinarySearch<T, U>(node->Right());
-    if (!other.first) return other;
-    if (other.second.first <= node->Value()) return {false, {}};
-    range.first = std::min(range.first, other.second.first);
-    range.second = std::max(range.second, other.second.second);
-  }
-  return {true, range};
+bool BinarySearch(const U* node, const T* minimum, const T* maximum) {
+  if (!node) return true;
+  if (minimum && node->Value() <= *minimum) return false;
+  if (maximum && node->Value() > *maximum) return false;
+  return BinarySearch(node->Left(), minimum, &node->Value()) &&
+         BinarySearch(node->Right(), &node->Value(), maximum);
 }
 
 } // namespace internal
@@ -33,7 +19,7 @@ std::pair<bool, std::pair<T, T>> BinarySearch(const U* node) {
 // Implement a function to check if a binary tree is a binary search tree.
 template <typename T, typename U>
 bool BinarySearch(const U& tree) {
-  return !tree.Root() || internal::BinarySearch<T>(tree.Root()).first;
+  return internal::BinarySearch<T>(tree.Root(), nullptr, nullptr);
 }
 
 }
