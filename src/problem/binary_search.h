55e8728b1ce95b053ac45a295e33ae7b97c2523a
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-31 11:24:58 +0100

    problem: Simplify the solution to the binary-search problem

--- src/problem/binary_search.h
+++ src/problem/binary_search.h
@@ -2,48 +2,30 @@
 #define PROBLEM_BINARY_SEARCH_H_
 
 #include <algorithm>
-#include <limits>
 #include <utility>
 
 namespace problem {
 
 namespace internal {
 
-enum class Type {
-  Abort,
-  Empty,
-  Range,
-};
-
-template <typename T>
-using Range = std::pair<T, T>;
-
-template <typename T>
-using Result = std::pair<Type, Range<T>>;
-
 template <typename T, typename U>
-Result<T> BinarySearch(const U* node) {
-  if (!node) return {Type::Empty, {}};
-  Range<T> range = {node->Value(), node->Value()};
-  {
+std::pair<bool, std::pair<T, T>> BinarySearch(const U* node) {
+  auto range = std::make_pair(node->Value(), node->Value());
+  if (node->Left()) {
     auto other = BinarySearch<T, U>(node->Left());
-    if (other.first == Type::Abort) return other;
-    else if (other.first == Type::Range) {
-      if (other.second.second > node->Value()) return {Type::Abort, {}};
-      range.first = std::min(range.first, other.second.first);
-      range.second = std::max(range.second, other.second.second);
-    }
+    if (!other.first) return other;
+    if (other.second.second > node->Value()) return {false, {}};
+    range.first = std::min(range.first, other.second.first);
+    range.second = std::max(range.second, other.second.second);
   }
-  {
+  if (node->Right()) {
     auto other = BinarySearch<T, U>(node->Right());
-    if (other.first == Type::Abort) return other;
-    else if (other.first == Type::Range) {
-      if (other.second.first <= node->Value()) return {Type::Abort, {}};
-      range.first = std::min(range.first, other.second.first);
-      range.second = std::max(range.second, other.second.second);
-    }
+    if (!other.first) return other;
+    if (other.second.first <= node->Value()) return {false, {}};
+    range.first = std::min(range.first, other.second.first);
+    range.second = std::max(range.second, other.second.second);
   }
-  return {Type::Range, range};
+  return {true, range};
 }
 
 } // namespace internal
@@ -51,7 +33,7 @@ Result<T> BinarySearch(const U* node) {
 // Implement a function to check if a binary tree is a binary search tree.
 template <typename T, typename U>
 bool BinarySearch(const U& tree) {
-  return internal::BinarySearch<T>(tree.Root()).first != internal::Type::Abort;
+  return !tree.Root() || internal::BinarySearch<T>(tree.Root()).first;
 }
 
 }
