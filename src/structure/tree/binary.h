bc9dd9f228653f42de596be895554b7675f23637
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-12-31 19:27:56 +0100

    structure/tree: Extract Binary from BinarySearch

---
+++ src/structure/tree/binary.h
@@ -0,0 +1,74 @@
+#ifndef STRUCTURE_TREE_BINARY_H_
+#define STRUCTURE_TREE_BINARY_H_
+
+#include <memory>
+#include <utility>
+
+namespace structure { namespace tree {
+
+template <typename T>
+class Binary {
+ public:
+  class Node;
+
+  Node* Root() const {
+    return root_.get();
+  }
+
+ protected:
+  std::unique_ptr<Node>& RootOwned() {
+    return root_;
+  }
+
+  std::unique_ptr<Node>& LeftOwned(std::unique_ptr<Node>& node) const {
+    return node->left_;
+  }
+
+  std::unique_ptr<Node>& RightOwned(std::unique_ptr<Node>& node) const {
+    return node->right_;
+  }
+
+ private:
+  std::unique_ptr<Node> root_;
+};
+
+template <typename T>
+class Binary<T>::Node {
+  friend class Binary;
+
+ public:
+  Node(T value) : value_(std::move(value)) {}
+
+  const T& Value() const {
+    return value_;
+  }
+
+  Node* Left() const {
+    return left_.get();
+  }
+
+  Node* Right() const {
+    return right_.get();
+  }
+
+  template <typename U>
+  bool Accept(U& visitor) const;
+
+ private:
+  T value_;
+  std::unique_ptr<Node> left_;
+  std::unique_ptr<Node> right_;
+};
+
+template <typename T>
+template <typename U>
+bool Binary<T>::Node::Accept(U& visitor) const {
+  if (left_ && !left_->Accept(visitor)) return false;
+  if (!visitor.Visit(this)) return false;
+  if (right_ && !right_->Accept(visitor)) return false;
+  return true;
+}
+
+} } // namespace structure::tree
+
+#endif // STRUCTURE_TREE_BINARY_H_
--- src/structure/tree/search.h
+++ src/structure/tree/search.h
@@ -1,90 +1,40 @@
 #ifndef STRUCTURE_TREE_SEARCH_H_
 #define STRUCTURE_TREE_SEARCH_H_
 
-#include <memory>
-#include <utility>
+#include "structure/tree/binary.h"
 
 namespace structure { namespace tree {
 
 template <typename T>
-class BinarySearch {
+class BinarySearch: public Binary<T> {
  public:
-  class Node;
+  using Node = typename Binary<T>::Node;
 
-  Node* Root() const {
-    return root_.get();
-  }
-
-  BinarySearch& Insert(std::unique_ptr<Node> node);
-
-  BinarySearch& Insert(T value) {
-    return Insert(std::unique_ptr<Node>(new Node(std::move(value))));
-  }
+  BinarySearch& Insert(T value);
 
   Node* Search(const T& value) const;
-
- private:
-  std::unique_ptr<Node> root_;
 };
 
 template <typename T>
-class BinarySearch<T>::Node {
-  friend class BinarySearch;
-
- public:
-  const T& Value() const {
-    return value_;
-  }
-
-  Node* Left() const {
-    return left_.get();
-  }
-
-  Node* Right() const {
-    return right_.get();
-  }
-
-  template <typename U>
-  bool Accept(U& visitor) const;
-
- private:
-  Node(T value) : value_(std::move(value)) {}
-
-  T value_;
-  std::unique_ptr<Node> left_;
-  std::unique_ptr<Node> right_;
-};
-
-template <typename T>
-BinarySearch<T>& BinarySearch<T>::Insert(std::unique_ptr<Node> node) {
-  auto* target = &root_;
+BinarySearch<T>& BinarySearch<T>::Insert(T value) {
+  auto target = &this->RootOwned();
   while (*target) {
-    auto parent = target->get();
-    if (parent->value_ < node->value_) target = &parent->right_;
-    else target = &parent->left_;
+    if ((*target)->Value() < value) target = &this->RightOwned(*target);
+    else target = &this->LeftOwned(*target);
   }
-  *target = std::move(node);
+  *target = std::unique_ptr<Node>(new Node(std::move(value)));
   return *this;
 }
 
 template <typename T>
 typename BinarySearch<T>::Node* BinarySearch<T>::Search(const T& value) const {
-  auto current = this->root_.get();
+  auto current = this->Root();
   while (current) {
-    if (value < current->value_) current = current->Left();
-    else if (current->value_ < value) current = current->Right();
+    if (value < current->Value()) current = current->Left();
+    else if (current->Value() < value) current = current->Right();
     else return current;
   }
   return nullptr;
-}
-
-template <typename T>
-template <typename U>
-bool BinarySearch<T>::Node::Accept(U& visitor) const {
-  if (left_ && !left_->Accept(visitor)) return false;
-  if (!visitor.Visit(this)) return false;
-  if (right_ && !right_->Accept(visitor)) return false;
-  return true;
 }
 
 } } // namespace structure::tree
