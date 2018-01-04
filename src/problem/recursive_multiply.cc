4a3bb57c789c49cf8e0c99d705486b3c20192660
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-04 20:22:19 +0100

    problem: Solve the recursive-multiply problem

--- src/problem/recursive_multiply.cc
+++ src/problem/recursive_multiply.cc
@@ -1,5 +1,16 @@
 #include "problem/recursive_multiply.h"
 
+namespace internal {
+
+std::size_t RecursiveMultiply(std::size_t one, std::size_t other) {
+  if (other == 1) return one;
+  auto result = RecursiveMultiply(one, other >> 1) << 1;
+  return (((other >> 1) << 1) != other) ? result + one : result;
+}
+
+} // namespace internal
+
 std::size_t problem::RecursiveMultiply(std::size_t one, std::size_t other) {
-  return 0;
+  return (one < other) ? internal::RecursiveMultiply(other, one)
+                       : internal::RecursiveMultiply(one, other);
 }
