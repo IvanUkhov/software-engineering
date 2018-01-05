78d719c0e2e1fede1b1dd8f66e65d2edacc76fd7
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-05 09:29:37 +0100

    problem: Solve the string-permutations problem with nonunique chars

--- src/problem/string_permutations.cc
+++ src/problem/string_permutations.cc
@@ -1,19 +1,32 @@
+#include <cassert>
 #include <cstddef>
+#include <unordered_map>
 #include <utility>
 
 #include "problem/string_permutations.h"
 
-using std::size_t;
-
-void StringPermutationsUniqueChars(std::string& string, size_t begin,
-                                   size_t end, std::ostream& output) {
+void StringPermutationsUniqueChars(std::string& string, std::size_t begin,
+                                   std::size_t end, std::ostream& output) {
   using std::swap;
-  if (begin + 1 == end) output << string << '\n';
-  else {
-    for (size_t i = begin; i < end; ++i) {
-      swap(string[begin], string[i]);
-      StringPermutationsUniqueChars(string, begin + 1, end, output);
-      swap(string[begin], string[i]);
+  for (std::size_t i = begin; i < end; ++i) {
+    swap(string[begin], string[i]);
+    if (begin + 2 >= end) output << string << '\n';
+    else StringPermutationsUniqueChars(string, begin + 1, end, output);
+    swap(string[begin], string[i]);
+  }
+}
+
+void StringPermutationsNonuniqueChars(
+    std::string& string, std::size_t begin, std::size_t end,
+    std::unordered_map<char, std::size_t>& choice, std::ostream& output) {
+  for (auto& pair : choice) {
+    if (pair.second == 0) continue;
+    string[begin] = pair.first;
+    if (begin + 1 >= end) output << string << '\n';
+    else {
+      --pair.second;
+      StringPermutationsNonuniqueChars(string, begin + 1, end, choice, output);
+      ++pair.second;
     }
   }
 }
@@ -25,4 +38,7 @@ void problem::StringPermutationsUniqueChars(std::string string,
 
 void problem::StringPermutationsNonuniqueChars(std::string string,
                                                std::ostream& output) {
+  std::unordered_map<char, std::size_t> choice;
+  for (auto glyph : string) ++choice[glyph];
+  ::StringPermutationsNonuniqueChars(string, 0, string.size(), choice, output);
 }
--- test/problem/string_permutations.cc
+++ test/problem/string_permutations.cc
@@ -2,11 +2,22 @@
 #include "gtest/gtest.h"
 #include "problem/string_permutations.h"
 
-TEST(ProblemTest, StringPermutationsUniqueChars) {
+namespace {
+
+template <typename T>
+void Assess(T process, const std::string& input, const std::string& output) {
   std::stringstream buffer;
-  problem::StringPermutationsUniqueChars("abc", buffer);
+  process(input, buffer);
   std::string actual = fixture::Sort(buffer.str());
-  std::string expected = fixture::Clean(R"(
+  std::string expected = fixture::Clean(output);
+  ASSERT_EQ(actual, expected);
+}
+
+}
+
+TEST(ProblemTest, StringPermutationsUniqueChars) {
+  Assess(problem::StringPermutationsUniqueChars, "a", "a");
+  Assess(problem::StringPermutationsUniqueChars, "abc", R"(
 abc
 acb
 bac
@@ -14,8 +25,22 @@ bca
 cab
 cba
 )");
-  ASSERT_EQ(actual, expected);
 }
 
 TEST(ProblemTest, StringPermutationsNonuniqueChars) {
+  Assess(problem::StringPermutationsNonuniqueChars, "a", "a");
+  Assess(problem::StringPermutationsNonuniqueChars, "aabc", R"(
+aabc
+aacb
+abac
+abca
+acab
+acba
+baac
+baca
+bcaa
+caab
+caba
+cbaa
+)");
 }
