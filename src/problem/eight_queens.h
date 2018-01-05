8f45a5e7ae9313ca4c7a29a6118b358454da2e95
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-05 16:20:33 +0100

    problem: Solve the eight-queens problem

--- src/problem/eight_queens.h
+++ src/problem/eight_queens.h
@@ -1,8 +1,8 @@
 #ifndef PROBLEM_EIGHT_QUEENS_H_
 #define PROBLEM_EIGHT_QUEENS_H_
 
+#include <algorithm>
 #include <cstddef>
-#include <ostream>
 
 namespace problem {
 
@@ -11,7 +11,51 @@ namespace problem {
 // this case, “diagonal” means all diagonals, not just the two that bisect the
 // board.
 template <std::size_t N = 8>
-void EightQueens(std::ostream& output) {
+class EightQueens {
+ public:
+  using Board = bool[N][N];
+
+ protected:
+  void Start();
+  virtual void Commit(const Board& board) = 0;
+
+ private:
+  static const std::size_t M = 2 * N - 1;
+
+  void Process(std::size_t i);
+
+  void Reset() {
+    std::fill(&board_[0][0], &board_[N][0], false);
+    std::fill(&block_[0], &block_[N + M + M], false);
+  }
+
+  Board board_;
+  bool block_[N + M + M];
+};
+
+template <std::size_t N>
+void EightQueens<N>::Start() {
+  Reset();
+  Process(0);
+}
+
+template <std::size_t N>
+void EightQueens<N>::Process(std::size_t i) {
+  for (std::size_t j = 0; j < N; ++j) {
+    auto k = N + i + j;
+    auto l = N + M + i + N - 1 - j;
+    if (block_[j] || block_[k] || block_[l]) continue;
+    board_[i][j] = true;
+    block_[j] = true;
+    block_[k] = true;
+    block_[l] = true;
+    if (i + 1 == N) Commit(board_);
+    else Process(i + 1);
+    board_[i][j] = false;
+    block_[j] = false;
+    block_[k] = false;
+    block_[l] = false;
+  }
 }
 
 } // namespace problem
--- test/problem/eight_queens.cc
+++ test/problem/eight_queens.cc
@@ -1,9 +1,25 @@
-#include <sstream>
-
 #include "gtest/gtest.h"
 #include "problem/eight_queens.h"
 
+class EightQueens: protected problem::EightQueens<> {
+ public:
+  EightQueens() {
+    Start();
+  }
+
+  std::size_t Count() const {
+    return count_;
+  }
+
+ protected:
+  void Commit(const Board& board) override {
+    ++count_;
+  }
+
+ private:
+  std::size_t count_ = 0;
+};
+
 TEST(ProblemTest, EightQueens) {
-  std::stringstream output;
-  problem::EightQueens(output);
+  ASSERT_EQ(EightQueens().Count(), 92);
 }
