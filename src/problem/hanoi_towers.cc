a470d2d69da5e652b0411897312395220bb16791
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-06 19:15:16 +0100

    problem: Turn a closure into a function

--- src/problem/hanoi_towers.cc
+++ src/problem/hanoi_towers.cc
@@ -1,16 +1,21 @@
 #include "problem/hanoi_towers.h"
 
-void problem::HanoiTowers(Stack& a, Stack& b, Stack& c) {
-  auto slide = [](Stack& one, Stack& other) {
-    if (one.IsEmpty() && other.IsEmpty()) return;
-    else if (one.IsEmpty()) one.Push(other.Pop());
-    else if (other.IsEmpty()) other.Push(one.Pop());
-    else if (one.Peek() > other.Peek()) one.Push(other.Pop());
-    else other.Push(one.Pop());
-  };
+namespace problem {
+
+static void Slide(Stack& one, Stack& other) {
+  if (one.IsEmpty() && other.IsEmpty()) return;
+  else if (one.IsEmpty()) one.Push(other.Pop());
+  else if (other.IsEmpty()) other.Push(one.Pop());
+  else if (one.Peek() > other.Peek()) one.Push(other.Pop());
+  else other.Push(one.Pop());
+}
+
+void HanoiTowers(Stack& a, Stack& b, Stack& c) {
   while (!a.IsEmpty() || !b.IsEmpty()) {
-    slide(a, b);
-    slide(a, c);
-    slide(b, c);
+    Slide(a, b);
+    Slide(a, c);
+    Slide(b, c);
   }
 }
+
+} // namespace problem
