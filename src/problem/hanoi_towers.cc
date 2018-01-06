f47b44320563083bf738e56d1d537d4858d7a2cc
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-06 13:57:04 +0100

    problem: Solve the hanoi-towers problem

--- src/problem/hanoi_towers.cc
+++ src/problem/hanoi_towers.cc
@@ -1,4 +1,16 @@
 #include "problem/hanoi_towers.h"
 
 void problem::HanoiTowers(std::size_t count, Stack& a, Stack& b, Stack& c) {
+  auto slide = [](Stack& one, Stack& other) {
+    if (one.IsEmpty() && other.IsEmpty()) return;
+    else if (one.IsEmpty()) one.Push(other.Pop());
+    else if (other.IsEmpty()) other.Push(one.Pop());
+    else if (one.Peek() > other.Peek()) one.Push(other.Pop());
+    else other.Push(one.Pop());
+  };
+  while (!a.IsEmpty() || !b.IsEmpty()) {
+    slide(a, b);
+    slide(a, c);
+    slide(b, c);
+  }
 }
--- test/problem/hanoi_towers.cc
+++ test/problem/hanoi_towers.cc
@@ -1,17 +1,31 @@
+#include <vector>
+
 #include "gtest/gtest.h"
 #include "problem/hanoi_towers.h"
 
 using problem::Stack;
+using Vector = std::vector<std::size_t>;
 
-static const std::size_t kCount = 10;
+static Vector Drain(Stack& stack) {
+  Vector vector;
+  while (!stack.IsEmpty()) vector.push_back(stack.Pop());
+  return vector;
+}
+
+void Assess(std::size_t count) {
+  Stack a, b, c;
+  Vector expected;
+  for (std::size_t i = 0; i < count; ++i) {
+    a.Push(count - i);
+    expected.push_back(i + 1);
+  }
+  problem::HanoiTowers(count, a, b, c);
+  ASSERT_EQ(Drain(a), Vector());
+  ASSERT_EQ(Drain(b), Vector());
+  ASSERT_EQ(Drain(c), expected);
+}
 
 TEST(ProblemTest, HanoiTowers) {
-  Stack a;
-  for (std::size_t i = 0; i < kCount; ++i) a.Push(kCount - 1);
-  Stack b, c;
-  problem::HanoiTowers(kCount, a, b, c);
-  ASSERT_TRUE(a.IsEmpty());
-  ASSERT_TRUE(b.IsEmpty());
-  for (std::size_t i = 0; i < kCount; ++i) ASSERT_EQ(c.Pop(), i + 1);
-  ASSERT_TRUE(c.IsEmpty());
+  Assess(10);
+  Assess(11);
 }
