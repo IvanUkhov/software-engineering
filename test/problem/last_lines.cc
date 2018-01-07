e5a4f70cdb7e8a59de6775a50746f6db97b253ad
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-07 17:03:33 +0100

    problem: Refactor a couple of tests

--- test/problem/last_lines.cc
+++ test/problem/last_lines.cc
@@ -3,26 +3,30 @@
 #include "gtest/gtest.h"
 #include "problem/last_lines.h"
 
-static const std::size_t kCount = 10;
+static const std::size_t kLast = 3;
+static const std::size_t kTotal = 10;
+
+std::string Enumerate(std::size_t begin, std::size_t end) {
+  std::stringstream output;
+  for (std::size_t i = begin; i < end; ++i) output << i << '\n';
+  return output.str();
+}
 
 TEST(ProblemTest, LastLinesWithFinalNewLine) {
-  std::stringstream input, output;
-  for (std::size_t i = kCount; i > 0; --i) input << i << '\n';
-  problem::LastLines(3, input, output);
-  ASSERT_EQ(output.str(), "3\n2\n1\n");
+  std::stringstream input(Enumerate(0, kTotal)), output;
+  problem::LastLines(kLast, input, output);
+  ASSERT_EQ(output.str(), Enumerate(kTotal - kLast, kTotal));
 }
 
 TEST(ProblemTest, LastLinesWithoutFinalNewLine) {
-  std::stringstream input, output;
-  for (std::size_t i = kCount; i > 0; --i) input << i << '\n';
+  std::stringstream input(Enumerate(0, kTotal)), output;
   input.str(input.str().substr(0, static_cast<std::size_t>(input.tellp()) - 1));
-  problem::LastLines(3, input, output);
-  ASSERT_EQ(output.str(), "3\n2\n1\n");
+  problem::LastLines(kLast, input, output);
+  ASSERT_EQ(output.str(), Enumerate(kTotal - kLast, kTotal));
 }
 
 TEST(ProblemTest, LastLinesTooFew) {
-  std::stringstream input, output;
-  for (std::size_t i = kCount; i > 0; --i) input << i << '\n';
-  problem::LastLines(2 * kCount, input, output);
-  ASSERT_EQ(output.str(), "10\n9\n8\n7\n6\n5\n4\n3\n2\n1\n");
+  std::stringstream input(Enumerate(0, kTotal)), output;
+  problem::LastLines(2 * kTotal, input, output);
+  ASSERT_EQ(output.str(), Enumerate(0, kTotal));
 }
