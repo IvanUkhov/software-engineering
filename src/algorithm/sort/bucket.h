139cfea66990d1583118cbac68cc80b93c3d30b3
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-08 08:12:45 +0100

    algorithm/sort: Do not do modulo in Bucket

--- src/algorithm/sort/bucket.h
+++ src/algorithm/sort/bucket.h
@@ -14,7 +14,7 @@ template <typename T, void Sort(std::vector<T>&) = Insertion<T>>
 void Bucket(std::vector<T>& data, std::size_t granularity,
             std::function<std::size_t(const T&)> map) {
   std::vector<std::vector<T>> buckets(granularity);
-  for (auto& value : data) buckets[map(value) % granularity].push_back(value);
+  for (auto& value : data) buckets[map(value)].push_back(value);
   auto iterator = data.begin();
   for (auto& bucket : buckets) {
     Sort(bucket);
