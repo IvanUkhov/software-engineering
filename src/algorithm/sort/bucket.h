7e9bb7a362c88abcdae4c8693dee3a6ec3a89367
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-08 08:07:46 +0100

    algorithm/sort: Allow a different sorting method inside Bucket

--- src/algorithm/sort/bucket.h
+++ src/algorithm/sort/bucket.h
@@ -10,14 +10,14 @@
 
 namespace algorithm { namespace sort {
 
-template <typename T>
+template <typename T, void Sort(std::vector<T>&) = Insertion<T>>
 void Bucket(std::vector<T>& data, std::size_t granularity,
             std::function<std::size_t(const T&)> map) {
   std::vector<std::vector<T>> buckets(granularity);
   for (auto& value : data) buckets[map(value) % granularity].push_back(value);
   auto iterator = data.begin();
   for (auto& bucket : buckets) {
-    Insertion(bucket);
+    Sort(bucket);
     std::copy(bucket.begin(), bucket.end(), iterator);
     iterator += bucket.size();
   }
